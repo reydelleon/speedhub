3ce88d437ea01c2da2d262111c801424e26a9b6a
Reydel Leon Machado <contact@reydelleon.me>
2015-01-09 12:47:12 -0500

    Initial Import.

---
+++ app/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//var app = angular.module('core', []);
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.browserAction.setBadgeText({ text: '\'Allo' });
+
+console.log('\'Allo \'Allo! Event Page for Browser Action');
